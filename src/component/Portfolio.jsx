e356bd8554d0a35988ec57869ac301664e94081d
freelancerjunayed junayed <freelancerjunayedjunayed@gmail.com>
2026-02-13 23:59:51 +0600

    added items

--- src/component/Portfolio.jsx
+++ src/component/Portfolio.jsx
@@ -7,10 +7,10 @@ const Portfolio = () => {
             <h2 className="text-3xl font-normal text-center py-16">Portfolio</h2>
             <p className="text-center font-normal font-regular mb-8">A showcase of my recent photography work.</p>
           <div>
-            <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4'>
-                <img src={img} alt="Portfolio 1" className="rounded-lg shadow-md" />
-                <img src={img} alt="Portfolio 2" className="rounded-lg shadow-md" />
-                <img src={img} alt="Portfolio 3" className="rounded-lg shadow-md" />
+            <div className='flex  gap-4 items-center justify-center'>
+                <img src={img} alt="Portfolio 1" className="rounded-lg shadow-md w-full md:w-[340px] h-full object-cover" />
+                <img src={img} alt="Portfolio 2" className="rounded-lg shadow-md w-full md:w-[540px] h-full object-cover" />
+                <img src={img} alt="Portfolio 3" className="rounded-lg shadow-md w-full md:w-[340px] h-full md:h-[412px] object-cover" />
 
             </div>
             <div>
