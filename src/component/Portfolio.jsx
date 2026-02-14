4e1dfb17a1087ad977e359d861e0469119bd356a
freelancerjunayed junayed <freelancerjunayedjunayed@gmail.com>
2026-02-14 23:55:21 +0600

    added files

--- src/component/Portfolio.jsx
+++ src/component/Portfolio.jsx
@@ -9,6 +9,8 @@ import wedd3 from "../../src/assets/wedding3.jfif"
 import wedd4 from "../../src/assets/wedding4.jfif"
 import wedd5 from "../../src/assets/wedding5.jfif"
 import wedd6 from "../../src/assets/wedding6.jfif"
+import nature from "../../src/assets/Nature.jfif"
+import nature1 from "../../src/assets/Nature1.png"
 
 
 const Portfolio = () => {
@@ -58,6 +60,36 @@ const Portfolio = () => {
                
                 <div className="overflow-hidden rounded-2xl">
                      <img src={wedd2} alt="Portfolio 2" className="rounded-lg shadow-md w-full md:h-[412px] md:w-[433px] h-full object-cover hidden   md:block hover:scale-110 transition duration-300" />
+                </div>        
+                <div className="overflow-hidden rounded-2xl">
+                    <img src={wedd3} alt="Portfolio 3" className="rounded-lg shadow-md w-full  md:w-[288px] h-full md:h-[412px] object-cover hover:scale-110 transition duration-300" />
+                </div>
+
+            </div>
+            <div className='flex gap-3 mt-3 items-center justify-center'>
+                <div className="overflow-hidden rounded-2xl">
+                <img src={wedd4} alt="Portfolio 4" className="rounded-lg shadow-md w-full md:h-[412px] md:w-[456px] h-full object-cover hover:scale-110 transition duration-300" />
+                </div>
+
+                <div className="overflow-hidden rounded-2xl">
+                     <img src={wedd5} alt="Portfolio 5" className="rounded-lg shadow-md w-full md:h-[412px] md:w-[562px] h-full object-cover md:block hidden hover:scale-110 transition duration-300" />
+                </div>
+            </div>
+            <div>
+
+            </div>
+          </div>
+
+          {/* Nature Photography */}
+               <h2 className="text-2xl font-normal text-center py-8">Nature Photography</h2>
+          <div>
+            <div className='md:flex  gap-3 items-center justify-center'>
+                <div className="overflow-hidden rounded-2xl">
+                     <img src={nature} alt="Portfolio 2" className="rounded-lg shadow-md w-full md:h-[412px] md:w-[300px] h-full object-cover hidden   md:block hover:scale-110 transition duration-300" />
+                </div>
+               
+                <div className="overflow-hidden rounded-2xl">
+                     <img src={nature1} alt="Portfolio 2" className="rounded-lg shadow-md w-full md:h-[412px] md:w-[433px] h-full object-cover hidden   md:block hover:scale-110 transition duration-300" />
                 </div>        
                 <div className="overflow-hidden rounded-2xl">
                     <img src={wedd3} alt="Portfolio 3" className="rounded-lg shadow-md w-full  md:w-[288px] h-full md:h-[412px] object-cover hover:scale-110 transition duration-300" />
